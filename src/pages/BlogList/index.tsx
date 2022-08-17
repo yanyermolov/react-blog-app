d1b5e7b8a22ce21039928d1608ef87855e7c7814
YanYermolov <yermolovyan@gmail.com>
2022-08-17 17:53:59 +0300

    change index.js

--- src/pages/BlogList/index.tsx
+++ src/pages/BlogList/index.tsx
@@ -1,19 +1,33 @@
-import { useState } from "react";
-import { IPost } from "../../models/models";
+import { useEffect, useState } from 'react'
+import { IPost } from '../../models/models'
+import { listPosts } from '../../actions/posts'
 
 const BlogList = () => {
-    const [posts, setPosts] = useState<IPost[]>([])
+  const [posts, setPosts] = useState<IPost[]>([])
 
-    return ( 
-        <div>
-            {posts?.map((post: IPost) => (
-                <div key={post?.id}>
-                    <h2>{post.title}</h2>
-                    <p>Comments</p>
-                </div>
-            ))}
+  const handlePosts = async () => {
+    try {
+      const res = await listPosts()
+      setPosts(res)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    handlePosts()
+  }, [])
+
+  return (
+    <div>
+      {posts?.map((post: IPost) => (
+        <div key={post?.id}>
+          <h2>{post.title}</h2>
+          <p>Comments</p>
         </div>
-     );
+      ))}
+    </div>
+  )
 }
 
 export default BlogList
